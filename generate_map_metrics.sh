b9fadd43d43f49ca0d1dfc8e0a12199514f56e37
Rishabh Mahajani <rmahajani31@gmail.com>
2023-05-31 20:11:00 +0530

    integrated the submission code with the mAP calculation code

--- generate_map_metrics.sh
+++ generate_map_metrics.sh
@@ -1,10 +1,11 @@
-export BOUNDING_BOXES=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_bbox.csv
-export IMAGE_LABELS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_labels.csv
-export INPUT_PREDICTIONS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/seg_preds.csv
-export INSTANCE_SEGMENTATIONS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_masks.csv
-export OUTPUT_METRICS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/metrics.txt
+export SUFFIX=all_valid_preproc
+export BOUNDING_BOXES=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_bbox_${SUFFIX}.csv
+export IMAGE_LABELS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_labels_${SUFFIX}.csv
+export INPUT_PREDICTIONS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/seg_preds_${SUFFIX}.csv
+export INSTANCE_SEGMENTATIONS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_masks_${SUFFIX}.csv
+export OUTPUT_METRICS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/metrics_${SUFFIX}.txt
 
-python3 object_detection/metrics/oid_challenge_evaluation.py \
+python3 /home/rmahajani31/Projects/models/research/object_detection/metrics/oid_challenge_evaluation.py \
     --input_annotations_boxes=${BOUNDING_BOXES} \
     --input_annotations_labels=${IMAGE_LABELS} \
     --input_class_labelmap=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/label_map.pbtxt \
