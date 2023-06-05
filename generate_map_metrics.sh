3d75c17d382347ceb7888aefa6a61d6e843bc117
Rishabh Mahajani <rmahajani31@gmail.com>
2023-06-05 21:13:18 +0530

    created the hubmap trans unet submission fil

--- generate_map_metrics.sh
+++ generate_map_metrics.sh
@@ -1,9 +1,10 @@
-export SUFFIX=all_valid_preproc
-export BOUNDING_BOXES=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_bbox_${SUFFIX}.csv
-export IMAGE_LABELS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_labels_${SUFFIX}.csv
-export INPUT_PREDICTIONS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/seg_preds_${SUFFIX}.csv
-export INSTANCE_SEGMENTATIONS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_masks_${SUFFIX}.csv
-export OUTPUT_METRICS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/metrics_${SUFFIX}.txt
+export GROUND_TRUTH_SUFFIX=all_valid_preproc
+export PREDS_SUFFIX=all_valid_preproc
+export BOUNDING_BOXES=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_bbox_${GROUND_TRUTH_SUFFIX}.csv
+export IMAGE_LABELS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_labels_${GROUND_TRUTH_SUFFIX}.csv
+export INPUT_PREDICTIONS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/seg_preds_${PREDS_SUFFIX}.csv
+export INSTANCE_SEGMENTATIONS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/segmentation_masks_${GROUND_TRUTH_SUFFIX}.csv
+export OUTPUT_METRICS=/home/rmahajani31/Projects/hubmap-hacking-the-human-vasculature/map_input_data/metrics_${PREDS_SUFFIX}.txt
 
 python3 /home/rmahajani31/Projects/models/research/object_detection/metrics/oid_challenge_evaluation.py \
     --input_annotations_boxes=${BOUNDING_BOXES} \
