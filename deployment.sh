5e924615471249357364f03c422ab7e925d354b4
Akash Goyal <goyalakash391@gmail.com>
2023-06-05 13:17:04 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -26,3 +26,32 @@ sudo mkdir -p /mnt/disks/pre-installed
 sudo mount -o discard,defaults /dev/sdb /mnt/disks/pre-installed
 sudo chmod a+w /mnt/disks/
 
+------------------------------------------------
+aws
+
+lsblk
+sudo file -s /dev/xvdf
+If the above command output shows “/dev/xvdf: data“, it means your volume is empty.
+
+Step 6: Format the volume to the ext4 filesystem using the following command.
+sudo mkfs -t ext4 /dev/xvdf
+Alternatively, you can also use the xfs format. You have to use either ext4 or xfs.
+
+ sudo mkfs -t xfs /dev/xvdf
+Step 7: Create a directory of your choice to mount our new ext4 volume. I am using the name “newvolume“. You can name it something meaningful to you.
+
+sudo mkdir /pre-installed
+Step 8: Mount the volume to “newvolume” directory using the following command.
+
+sudo mount /dev/xvdf /pre-installed/
+Step 9: cd into newvolume directory and check the disk space to validate the volume mount.
+
+cd /pre-installed
+df -h .
+The above command should show the free space in the newvolume directory.
+
+To unmount the volume, use the unmount command as shown below..
+sudo umount /dev/xvdf
+
+sudo yum update
+yum install gcc gcc-c++ make 
