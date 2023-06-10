8fb33d9b1ef834520ca0c15836cb973080a6b14b
EC2 Default User <ec2-user@ip-172-31-16-92.ec2.internal>
2023-06-10 15:54:56 +0000

    committing latest training code

--- project_detectron2/detectron_training.py
+++ project_detectron2/detectron_training.py
@@ -43,7 +43,9 @@ import pickle
 from detectron2.structures import BoxMode
 from detectron2.data import DatasetMapper
 import detectron2.data.transforms as T
+from detectron2 import model_zoo
 import numpy as np
+
 
 # In[2]:
 
@@ -105,9 +107,10 @@ class ValidationDataset:
 
 
 # Function to register a dataset
+CLASSES = ['blood_vessel']
 def register_custom_dataset(dataset_name, data_dir):
     DatasetCatalog.register(dataset_name, lambda: HubMapDataset(data_dir))
-    MetadataCatalog.get(dataset_name).set(thing_classes=['blood_vessel'], evaluator_type="coco")
+    MetadataCatalog.get(dataset_name).set(thing_classes=CLASSES, evaluator_type="coco")
 
 
 # In[5]:
@@ -141,12 +144,22 @@ args = CustomArguments(**argument_dict)
 
 # Setup the config
 cfg = get_cfg()
-cfg.merge_from_file(args.config_file)
-cfg.merge_from_list(args.opts)
-cfg.freeze()
-default_setup(
-    cfg, args
-)
+cfg.merge_from_file(model_zoo.get_config_file('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'))
+cfg.DATASETS.TRAIN = (train_dataset_name,)
+cfg.DATASETS.TEST = ()
+cfg.INPUT.MIN_SIZE_TRAIN = (256,350,480,512)  # Minimum input image size during training
+cfg.INPUT.MAX_SIZE_TRAIN = 512     # Maximum input image size during training
+cfg.INPUT.MIN_SIZE_TEST = 512      # Minimum input image size during testing
+cfg.INPUT.MAX_SIZE_TEST = 512      # Maximum input image size during testing
+cfg.DATALOADER.NUM_WORKERS = 4
+# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml')
+cfg.MODEL.WEIGHTS = '/home/ec2-user/hubmap-hacking-the-human-vasculature/project_detectron2/output/inference/best_model_fold_0_with_added_aug_lr_0.00025.pth'
+cfg.SOLVER.IMS_PER_BATCH = 16
+cfg.SOLVER.BASE_LR = 0.02
+# cfg.SOLVER.BASE_LR = 0.002
+cfg.SOLVER.MAX_ITER = 90000
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(CLASSES)
+# cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 
 
 # In[8]:
@@ -154,15 +167,17 @@ default_setup(
 
 # compared to "train_net.py", we do not support accurate timing and
 # precise BN here, because they are not trivial to implement in a small training loop
+prob = 0.8
 data_transforms = [
-    T.RandomRotation([-90,90], expand=True),
-    T.RandomFlip(horizontal=True, vertical=False),
-    T.RandomFlip(horizontal=False, vertical=True),
-    T.RandomBrightness(0.8, 1.2),
-    T.RandomContrast(0.8, 1.2),
-    T.RandomSaturation(0.8,1.2),
-    T.RandomCrop(crop_type='relative_range', crop_size=(0.8,0.8))
+    T.RandomApply(T.RandomRotation([-45,45], expand=False), prob=prob),
+    T.RandomFlip(horizontal=True, vertical=False, prob=prob),
+    T.RandomFlip(horizontal=False, vertical=True, prob=prob),
+    T.RandomApply(T.RandomBrightness(0.8, 1.2), prob=prob),
+    T.RandomApply(T.RandomContrast(0.8, 1.2), prob=prob),
+    T.RandomApply(T.RandomSaturation(0.8,1.2), prob=prob),
+    T.RandomApply(T.RandomCrop('relative', (0.8, 0.8)), prob=prob)
 ]
+
 # data_loader = build_detection_train_loader(cfg, mapper=DatasetMapper(cfg, is_train=True, augmentations=data_transforms))
 
 
@@ -218,31 +233,37 @@ def get_evaluator(cfg, dataset_name, output_folder=None):
 # In[11]:
 
 
-# hubmap_dataset = HubMapDataset(train_dir)
-# num_folds = 5
-# kf = KFold(n_splits=num_folds)
-# val_indices = list(kf.split(hubmap_dataset))[0][1].tolist()
+# model = build_model(cfg)
+# model.train()
 
 
 # In[12]:
 
 
-# val_dataset = ValidationDataset(hubmap_dataset, val_indices)
-# val_dataset[0]['annotations'][0]['segmentation'][0][0]
-# DatasetCatalog.register(f'{train_dataset_name}-val-fold-0', lambda: ValidationDataset(hubmap_dataset, val_indices))
+# # for i in range(num_folds):
+# if os.path.exists(os.path.join(cfg.OUTPUT_DIR, "inference", f'{train_dataset_name}-val-fold-0')):
+#     shutil.rmtree(os.path.join(cfg.OUTPUT_DIR, "inference", f'{train_dataset_name}-val-fold-0'))
+# hubmap_dataset = DatasetCatalog.get(train_dataset_name)
+# DatasetCatalog.register(f'{train_dataset_name}-val-fold-0', lambda: ValidationDataset(hubmap_dataset, [1,2]))
 # MetadataCatalog.get(f'{train_dataset_name}-val-fold-0').set(thing_classes=['blood_vessel'], evaluator_type="coco")
 
 
 # In[13]:
 
 
-# import shutil
-# get_evaluator(
+# evaluator = get_evaluator(
 #         cfg, f'{train_dataset_name}-val-fold-0', os.path.join(cfg.OUTPUT_DIR, "inference", f'{train_dataset_name}-val-fold-0')
 #     )
+# data_loader = build_detection_test_loader(cfg, dataset_name=f'{train_dataset_name}-val-fold-0')
 
 
 # In[14]:
+
+
+# metrics = inference_on_dataset(model, data_loader, evaluator)
+
+
+# In[ ]:
 
 
 import time
@@ -267,23 +288,32 @@ if os.path.exists(os.path.join(cfg.OUTPUT_DIR, "inference", train_dataset_name))
 for i in range(num_folds):
     if os.path.exists(os.path.join(cfg.OUTPUT_DIR, "inference", f'{train_dataset_name}-val-fold-{i}')):
         shutil.rmtree(os.path.join(cfg.OUTPUT_DIR, "inference", f'{train_dataset_name}-val-fold-{i}'))
+    if os.path.exists(os.path.join(cfg.OUTPUT_DIR, "inference", f'{train_dataset_name}-train-fold-{i}')):
+        shutil.rmtree(os.path.join(cfg.OUTPUT_DIR, "inference", f'{train_dataset_name}-train-fold-{i}'))
 
 # Loop over the folds
 for fold, (train_indices, val_indices) in enumerate(kf.split(hubmap_dataset)):
+    train_indices = train_indices.tolist()
     val_indices = val_indices.tolist()
     # Create the training and validation data loaders for the current fold
     train_dataset = [hubmap_dataset[i] for i in train_indices]
+    DatasetCatalog.register(f'{train_dataset_name}-train-fold-{fold}', lambda: ValidationDataset(hubmap_dataset, train_indices))
+    MetadataCatalog.get(f'{train_dataset_name}-train-fold-{fold}').set(thing_classes=CLASSES, evaluator_type="coco")
     DatasetCatalog.register(f'{train_dataset_name}-val-fold-{fold}', lambda: ValidationDataset(hubmap_dataset, val_indices))
-    MetadataCatalog.get(f'{train_dataset_name}-val-fold-{fold}').set(thing_classes=['blood_vessel'], evaluator_type="coco")
+    MetadataCatalog.get(f'{train_dataset_name}-val-fold-{fold}').set(thing_classes=CLASSES, evaluator_type="coco")
 #     train_data_loader = build_detection_train_loader(cfg, mapper=DatasetMapper(cfg, is_train=True, augmentations=data_transforms), dataset=train_dataset)
     train_data_loader = build_detection_train_loader(cfg, mapper=DatasetMapper(cfg, is_train=True), dataset=train_dataset)
+    train_test_data_loader =  build_detection_test_loader(cfg, f'{train_dataset_name}-train-fold-{fold}')
     val_data_loader = build_detection_test_loader(cfg, f'{train_dataset_name}-val-fold-{fold}')
     evaluator = get_evaluator(
         cfg, f'{train_dataset_name}-val-fold-{fold}', os.path.join(cfg.OUTPUT_DIR, "inference", f'{train_dataset_name}-val-fold-{fold}')
     )
+    train_evaluator = get_evaluator(
+        cfg, f'{train_dataset_name}-train-fold-{fold}', os.path.join(cfg.OUTPUT_DIR, "inference", f'{train_dataset_name}-train-fold-{fold}')
+    )
     model = build_model(cfg)
     model.train()
-    resume = False
+    resume = True
     optimizer = build_optimizer(cfg, model)
     scheduler = build_lr_scheduler(cfg, optimizer)
     checkpointer = DetectionCheckpointer(
@@ -312,7 +342,7 @@ for fold, (train_indices, val_indices) in enumerate(kf.split(hubmap_dataset)):
                 if loss_key in loss_stats:
                     loss_stats[loss_key].append(loss_dict[loss_key].item())
             losses = sum(loss_dict.values())
-            loss_stats['total_loss'] = losses.item()
+            loss_stats['total_loss'].append(losses.item())
             assert torch.isfinite(losses).all(), loss_dict
 
             loss_dict_reduced = {k: v.item() for k, v in comm.reduce_dict(loss_dict).items()}
@@ -334,24 +364,34 @@ for fold, (train_indices, val_indices) in enumerate(kf.split(hubmap_dataset)):
             periodic_checkpointer.step(iteration)
         
             if (iteration+1) % iterations_per_epoch == 0:
+                train_metrics = inference_on_dataset(model, train_test_data_loader, train_evaluator)
                 metrics = inference_on_dataset(model, val_data_loader, evaluator)
+                print('===========')
+                print(train_metrics)
+                print(metrics)
+                print('===========')
                 metrics_str = ''
                 for task, task_metrics in metrics.items():
                     task_str = f'{task}: '
                     for metric, value in task_metrics.items():
                         task_str += f'{metric}={value:.4f}, '
                     metrics_str += task_str.rstrip(', ') + '\n'
+                for task, task_metrics in train_metrics.items():
+                    task_str = f'Training {task}: '
+                    for metric, value in task_metrics.items():
+                        task_str += f'{metric}={value:.4f}, '
+                    metrics_str += task_str.rstrip(', ') + '\n'
                 metrics_str = f'Epoch {epoch}, Iteration: {iteration}, time_taken: {float(time.time()-epoch_start_time)/60} minutes --> {metrics_str}'
-                epoch_start_time = time.time()
                 loss_str = ''
                 for loss_key in loss_stats.keys():
                     loss_str += f'{loss_key} - {np.mean(loss_stats[loss_key])}, '
                     loss_stats[loss_key] = []
-                with open(f'{output_dir}/model_stats_detectron_dataset1_fold_{fold}.txt', 'a') as f:
-                    f.write(f'{metrics_str}\n{loss_str}\n')
-                if metrics['segm']['AP50'] > max_ap_50:
+                if 'segm' in metrics and metrics['segm']['AP50'] > max_ap_50:
                     max_ap_50 = metrics['segm']['AP50']
                     torch.save(model.state_dict(), f'{output_dir}/best_model_fold_{fold}.pth')
                     with open(f'{output_dir}/best_model_detectron_dataset1_fold_{fold}.txt', 'w') as f:
                         f.write(f'{metrics_str}\n{loss_str}\n')
+                with open(f'{output_dir}/model_stats_detectron_dataset1_fold_{fold}.txt', 'a') as f:
+                    f.write(f'{metrics_str}\n{loss_str}\n')
+                epoch_start_time = time.time()
                 epoch += 1
