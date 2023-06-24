08e3e124f8ac22f25c3bb78f9e18723b4490d14f
EC2 Default User <ec2-user@ip-172-31-16-92.ec2.internal>
2023-06-24 07:56:11 +0000

    pushing latest yolo code

---
+++ project_yolo/yolo_training.py
@@ -0,0 +1,31 @@
+from ultralytics import YOLO
+import torch
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+model = YOLO('yolov8s-seg.pt')
+# model_config = {'data': 'hubmap.yaml', 'epochs': 100, 'imgsz': 512, 'save': True, 'save_period': 100, 'device': device, 'hsv_h': 0.0, 'hsv_s': 0.0, 'hsv_v': 0.0, 'translate': 0.0, 'scale': 0.0, 'flipud': 0.5, 'fliplr': 0.5, 'mosaic': 0.0, 'degrees': 0.0}
+
+custom_config = {
+ 'hsv_h': 0.015,
+ 'hsv_s': 0.7,
+ 'hsv_v': 0.4,
+ 'degrees': 0.0,
+ 'translate': 0.1,
+ 'scale': 0.9,
+ 'shear': 0.0,
+ 'perspective': 0.0,
+ 'flipud': 0.0,
+ 'fliplr': 0.5,
+ 'mosaic': 1.0,
+ 'mixup': 0.1,
+ 'copy_paste': 0.1}
+
+model_config = {'data': 'hubmap.yaml', 'epochs': 100, 'imgsz': 512, 'save': True, 'save_period': 5, 'plots': True, 'device': device}
+model_config.update(custom_config)
+model.train(**model_config)
+
+
+
+
