1f4e0663dec2864d565bfb4746442429c1f536bb
EC2 Default User <ec2-user@ip-172-31-30-79.ec2.internal>
2023-07-28 17:35:01 +0000

    co-detr

--- project_mmdet/configs/det/co-detr.py
+++ project_mmdet/configs/det/co-detr.py
@@ -2,39 +2,47 @@ _base_ = [
     './co_dino_5scale_r50_1x_coco.py'
 ]
  
+optimizer = dict(weight_decay=0.05)
 lr_config = dict(policy='step', step=[30])
 runner = dict(type='EpochBasedRunner', max_epochs=36)
 
 
-pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth'
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth'
 
-# model settings
 model = dict(
-    with_attn_mask=False,
     backbone=dict(
         _delete_=True,
         type='SwinTransformerV1',
-        drop_path_rate=0.5,
-        embed_dim=192,
+        embed_dim=96,
         depths=[2, 2, 18, 2],
-        num_heads=[6, 12, 24, 48],
-        out_indices=(0, 1, 2, 3),
-        window_size=12,
+        num_heads=[3, 6, 12, 24],
+        out_indices=(1, 2, 3),
+        window_size=7,
         ape=False,
+        drop_path_rate=0.4,
         patch_norm=True,
         use_checkpoint=False,
         pretrained=pretrained),
-    neck=dict(in_channels=[192, 192*2, 192*4, 192*8]),
-    query_head=dict(
-        transformer=dict(
-            encoder=dict(
-                # number of layers that use checkpoint
-                with_cp=6))))
+    neck=dict(in_channels=[96*2, 96*4, 96*8]))
 
+# model = dict(
+#     backbone=dict(
+#         _delete_=True,
+#         type='SwinTransformer',
+#         embed_dims=96,
+#         depths=[2, 2, 18, 2],
+#         num_heads=[3, 6, 12, 24],
+#         window_size=7,
+#         drop_path_rate=0.4,
+#         patch_norm=True,
+#         out_indices=(1, 2, 3),
+#         init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+#     neck=dict(in_channels=[96*2, 96*4, 96*8])
+# )
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-image_size = (768, 768)
+image_size = (1024, 1024)
 # load_pipeline = [
 #     dict(type='LoadImageFromFile'),
 #     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
@@ -55,21 +63,64 @@ image_size = (768, 768)
 #     dict(type='Pad', size=image_size, pad_val=dict(img=(114, 114, 114))),
 # ]
 
-load_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(
-        type='Resize',
-        img_scale=image_size,
-        keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-]
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# train_pipeline = [
+#     dict(type='LoadImageFromFile'),
+#     dict(type='LoadAnnotations', with_bbox=True),
+#     dict(type='Resize', img_scale=image_size, keep_ratio=True),
+#     dict(type='RandomFlip', flip_ratio=0.5),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='Pad', size_divisor=32),
+#     dict(type='DefaultFormatBundle'),
+#     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+# ]
 
 train_pipeline = [
-    dict(type='CopyPaste', max_num_pasted=100),
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='AutoAugment',
+        policies=[
+            [
+                dict(
+                    type='Resize',
+                    img_scale=[(480, 1333), (512, 1333), (544, 1333),
+                               (576, 1333), (608, 1333), (640, 1333),
+                               (672, 1333), (704, 1333), (736, 1333),
+                               (768, 1333), (800, 1333)],
+                    multiscale_mode='value',
+                    keep_ratio=True)
+            ],
+            [
+                dict(
+                    type='Resize',
+                    # The radio of all image in train dataset < 7
+                    # follow the original impl
+                    img_scale=[(400, 4200), (500, 4200), (600, 4200)],
+                    multiscale_mode='value',
+                    keep_ratio=True),
+                dict(
+                    type='RandomCrop',
+                    crop_type='absolute_range',
+                    crop_size=(384, 600),
+                    allow_negative_crop=True),
+                dict(
+                    type='Resize',
+                    img_scale=[(480, 1333), (512, 1333), (544, 1333),
+                               (576, 1333), (608, 1333), (640, 1333),
+                               (672, 1333), (704, 1333), (736, 1333),
+                               (768, 1333), (800, 1333)],
+                    multiscale_mode='value',
+                    override=True,
+                    keep_ratio=True)
+            ]
+        ]),
     dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=1),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
@@ -81,8 +132,9 @@ test_pipeline = [
             dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img'])
+            dict(type='Collect', keys=['img']),
         ])
 ]
 
@@ -116,17 +168,15 @@ num_classes = len(classes)  # Number of classes for classification
 data_root = f'/home/ec2-user/hubmap-hacking-the-human-vasculature/{base_data_dir_name_1}/{base_data_dir_name_2}{pseudo_label_name}_mmdet_fold_1/'
 dataset_type = 'CocoDataset'
 data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=1,
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
-        type='MultiImageMixDataset',
-        dataset=dict(
             type=dataset_type,
             classes=classes,
             ann_file=data_root + train_ann_file,
             img_prefix=data_root + train_data_prefix,
-            pipeline=load_pipeline),
-        pipeline=train_pipeline),
+            pipeline=train_pipeline
+        ),
     val=dict(
         type=dataset_type,
         ann_file=data_root + val_ann_file,
@@ -138,4 +188,4 @@ data = dict(
         img_prefix=data_root + val_data_prefix,
         pipeline=test_pipeline))
 
-load_from='/home/ec2-user/pretrained/co_dino_5scale_lsj_swin_large_3x_coco.pth'
+load_from='/home/ec2-user/pretrained/co_deformable_detr_swin_small_3x_coco.pth'
--- project_mmdet/configs/det/co_dino_5scale_r50_1x_coco.py
+++ project_mmdet/configs/det/co_dino_5scale_r50_1x_coco.py
@@ -15,7 +15,7 @@ model = dict(
         type='ResNet',
         depth=50,
         num_stages=4,
-        out_indices=(0, 1, 2, 3),
+        out_indices=(1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=False),
         norm_eval=True,
@@ -23,12 +23,12 @@ model = dict(
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
     neck=dict(
         type='ChannelMapper',
-        in_channels=[256, 512, 1024, 2048],
+        in_channels=[512, 1024, 2048],
         kernel_size=1,
         out_channels=256,
         act_cfg=None,
         norm_cfg=dict(type='GN', num_groups=32),
-        num_outs=5),
+        num_outs=4),
     rpn_head=dict(
         type='RPNHead',
         in_channels=256,
@@ -38,7 +38,7 @@ model = dict(
             octave_base_scale=4,
             scales_per_octave=3,
             ratios=[0.5, 1.0, 2.0],
-            strides=[4, 8, 16, 32, 64, 128]),
+            strides=[8, 16, 32, 64, 128]),
         bbox_coder=dict(
             type='DeltaXYWHBBoxCoder',
             target_means=[.0, .0, .0, .0],
@@ -47,40 +47,32 @@ model = dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0*num_dec_layer*lambda_2),
         loss_bbox=dict(type='L1Loss', loss_weight=1.0*num_dec_layer*lambda_2)),
     query_head=dict(
-        type='CoDINOHead',
-        num_query=900,
+        type='CoDeformDETRHead',
+        num_query=300,
         num_classes=num_classes,
-        num_feature_levels=5,
         in_channels=2048,
         sync_cls_avg_factor=True,
-        as_two_stage=True,
         with_box_refine=True,
+        as_two_stage=True,
         mixed_selection=True,
-        dn_cfg=dict(
-            type='CdnQueryGenerator',
-            noise_scale=dict(label=0.5, box=1.0),  # 0.5, 0.4 for DN-DETR
-            group_cfg=dict(dynamic=True, num_groups=None, num_dn_queries=100)),
         transformer=dict(
-            type='CoDinoTransformer',
-            with_pos_coord=True,
-            with_coord_feat=False,
+            type='CoDeformableDetrTransformer',
             num_co_heads=2,
-            num_feature_levels=5,
             encoder=dict(
                 type='DetrTransformerEncoder',
                 num_layers=6,
-                with_cp=4, # number of layers that use checkpoint
                 transformerlayers=dict(
                     type='BaseTransformerLayer',
                     attn_cfgs=dict(
-                        type='MultiScaleDeformableAttention', embed_dims=256, num_levels=5, dropout=0.0),
+                        type='MultiScaleDeformableAttention', embed_dims=256, dropout=0.0),
                     feedforward_channels=2048,
                     ffn_dropout=0.0,
                     operation_order=('self_attn', 'norm', 'ffn', 'norm'))),
             decoder=dict(
-                type='DinoTransformerDecoder',
-                num_layers=6,
+                type='CoDeformableDetrTransformerDecoder',
+                num_layers=num_dec_layer,
                 return_intermediate=True,
+                look_forward_twice=True,
                 transformerlayers=dict(
                     type='DetrTransformerDecoderLayer',
                     attn_cfgs=[
@@ -92,8 +84,7 @@ model = dict(
                         dict(
                             type='MultiScaleDeformableAttention',
                             embed_dims=256,
-                            num_levels=5,
-                            dropout=0.0),
+                            dropout=0.0)
                     ],
                     feedforward_channels=2048,
                     ffn_dropout=0.0,
@@ -102,13 +93,14 @@ model = dict(
         positional_encoding=dict(
             type='SinePositionalEncoding',
             num_feats=128,
-            temperature=20,
-            normalize=True),
+            normalize=True,
+            offset=-0.5),
         loss_cls=dict(
-            type='QualityFocalLoss',
+            type='FocalLoss',
             use_sigmoid=True,
-            beta=2.0,
-            loss_weight=1.0),
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=2.0),
         loss_bbox=dict(type='L1Loss', loss_weight=5.0),
         loss_iou=dict(type='GIoULoss', loss_weight=2.0)),
     roi_head=[dict(
@@ -117,8 +109,8 @@ model = dict(
             type='SingleRoIExtractor',
             roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
             out_channels=256,
-            featmap_strides=[4, 8, 16, 32, 64],
-            finest_scale=56),
+            featmap_strides=[8, 16, 32, 64],
+            finest_scale=112),
         bbox_head=dict(
             type='Shared2FCBBoxHead',
             in_channels=256,
@@ -145,7 +137,7 @@ model = dict(
             ratios=[1.0],
             octave_base_scale=8,
             scales_per_octave=1,
-            strides=[4, 8, 16, 32, 64, 128]),
+            strides=[8, 16, 32, 64, 128]),
         bbox_coder=dict(
             type='DeltaXYWHBBoxCoder',
             target_means=[.0, .0, .0, .0],
@@ -212,9 +204,7 @@ model = dict(
             pos_weight=-1,
             debug=False),],
     test_cfg=[
-        dict(
-            max_per_img=300,
-            nms=dict(type='soft_nms', iou_threshold=0.8)),
+        dict(max_per_img=100),
         dict(
             rpn=dict(
                 nms_pre=1000,
@@ -234,8 +224,7 @@ model = dict(
         # soft-nms is also supported for rcnn testing
         # e.g., nms=dict(type='soft_nms', iou_threshold=0.5, min_score=0.05)
     ])
-#find_unused_parameters = True
-#fp16 = dict(loss_scale=dict(init_scale=512))
+
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 # train_pipeline, NOTE the img_scale and the Pad's size_divisor is different
@@ -308,23 +297,21 @@ test_pipeline = [
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
-    train=dict(pipeline=train_pipeline),
+    train=dict(filter_empty_gt=False, pipeline=train_pipeline),
     val=dict(pipeline=test_pipeline),
     test=dict(pipeline=test_pipeline))
 # optimizer
 optimizer = dict(
     type='AdamW',
     lr=2e-4,
-    weight_decay=0.0001,
-    # custom_keys of sampling_offsets and reference_points in DeformDETR
-    paramwise_cfg=dict(custom_keys={'backbone': dict(lr_mult=0.1)}))
-
+    weight_decay=1e-4,
+    paramwise_cfg=dict(
+        custom_keys={
+            'backbone': dict(lr_mult=0.1),
+            'sampling_offsets': dict(lr_mult=0.1),
+            'reference_points': dict(lr_mult=0.1)
+        }))
 optimizer_config = dict(grad_clip=dict(max_norm=0.1, norm_type=2))
 # learning policy
 lr_config = dict(policy='step', step=[11])
 runner = dict(type='EpochBasedRunner', max_epochs=12)
-
-# NOTE: `auto_scale_lr` is for automatically scaling LR,
-# USER SHOULD NOT CHANGE ITS VALUES.
-# base_batch_size = (8 GPUs) x (2 samples per GPU)
-auto_scale_lr = dict(base_batch_size=16)
